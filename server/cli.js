a001d216a926ec2e588be6ab2e1e1d984ecd44d0
yohanmoore <yohanmoore@users.noreply.github.com>
2022-06-10 12:18:16 -0400

    bug fixes and new commands

--- server/cli.js
+++ server/cli.js
@@ -1,4 +1,5 @@
 import * as log from "https://deno.land/std@0.142.0/log/mod.ts";
+import { config } from "https://deno.land/std@0.142.0/dotenv/mod.ts";
 import { parse } from "https://deno.land/std@0.142.0/flags/mod.ts";
 import { runServer } from "file://C:/_GreenAntSolutions/Repositories/JSphereServer/jsphere/server.ts"
 
@@ -8,9 +9,9 @@ if (rootName != 'JSphereProjects') {
     Deno.exit(0);
 }
 
-const config = {
-    projects: {},
-    currentProject: ''
+const cliConfig = {
+    currentProject: '',
+    currentConfig: {}
 }
 
 let cmdLine;
@@ -19,9 +20,8 @@ while (cmdLine = prompt(promptText)) {
     const cmdArgs = parse(cmdLine.split(' '));
     try {
         switch (cmdArgs._[0]) {
-            case 'config': await processConfigCmd(cmdArgs); break;
+            case 'checkout': await processCheckoutCmd(cmdArgs); break;
             case 'create': await processCreateCmd(cmdArgs); break;
-            case 'init': await processInitCmd(cmdArgs); break;
             case 'quit': await processQuitCmd(cmdArgs); break;
             case 'reset': await processResetCmd(cmdArgs); break;
             case 'run': await processRunCmd(cmdArgs); break;
@@ -34,13 +34,37 @@ while (cmdLine = prompt(promptText)) {
     }
 }
 
-// CONFIG
-async function processConfigCmd(cmdArgs) {
+// CHECKOUT
+async function processCheckoutCmd(cmdArgs) {
     try {
-        if (config.currentProject) {
-            if (cmdArgs.username) config[config.currentProject].username = cmdArgs.username;
-            if (cmdArgs.accessToken) config[config.currentProject].username = cmdArgs.access-token;
-            log.info(`Updated configuration properties.`);
+        if (cliConfig.currentProject) {
+            const project = cliConfig.currentProject;
+            const provider = cliConfig.currentConfig.REMOTE_HOST;
+            const owner = cliConfig.currentConfig.REMOTE_ROOT;
+            const accessToken = cliConfig.currentConfig.REMOTE_AUTH;
+            const repo = cliConfig.currentConfig.REMOTE_CONFIG;
+            if (cmdArgs._[1].startsWith('.')) {
+                await cloneRepo({ project, provider, owner, accessToken, repo });
+            }
+            else {
+                const parts = cmdArgs._[1].split('/');
+                const appName = parts[0];
+                const appPackage = parts[1];
+                const appFile = await Deno.readFile(`${cliConfig.currentProject}/${repo}/.applications/${appName}.json`);
+                const appConfig = JSON.parse(new TextDecoder().decode(appFile));
+                if (appConfig.packages && appConfig.packages[appPackage]) {
+                    await cloneRepo({
+                        project,
+                        provider: appConfig.host.name,
+                        owner: appConfig.host.root,
+                        accessToken: appConfig.host.auth,
+                        repo: appPackage
+                    });
+                }
+                else {
+                    log.error(`The application '${appName}' does not have the package '${appPackage}' registered.`);
+                }
+            }
         }
         else log.error('This command is only valid within a project.')
     }
@@ -60,10 +84,11 @@ async function processCreateCmd(cmdArgs) {
 // CREATE PACKAGE
 async function processCreatePackageCmd(cmdArgs) {
     const packageName = cmdArgs._[2];
-    if (config.currentProject) {
+    if (cliConfig.currentProject) {
         if (packageName) {
-            await Deno.mkdir(`${config.currentProject}/${packageName}/client`, { recursive: true });
-            await Deno.mkdir(`${config.currentProject}/${packageName}/server`, { recursive: true });
+            await Deno.mkdir(`${cliConfig.currentProject}/${packageName}/client`, { recursive: true });
+            await Deno.mkdir(`${cliConfig.currentProject}/${packageName}/server`, { recursive: true });
+            await initRepo({ project: cliConfig.currentProject, repo: packageName });
         }
         else log.error(`Please provide a package name.`);
     }
@@ -76,44 +101,28 @@ async function processCreateProjectCmd(cmdArgs) {
     if (projectName) {
         await Deno.mkdir(`${projectName}`, { recursive: true });    
         const envSettings = {
-            projectHost: 'FileSystem',
-            projectRoot: '',
-            projectAuth: '',
-            projectConfig: '.jsphere'
+            useLocalConfig: 'true',
+            localRoot: projectName,
+            localConfig: '.jsphere'
         }
         await Deno.writeFile(`${projectName}/.env`, (new TextEncoder).encode(getEnvContent(envSettings)));
-        await processUseCmd({ _: [ 'use', projectName ] });
-        if (cmdArgs.app) {
-
+        if (cmdArgs.init) {
+            await Deno.mkdir(`${projectName}/.jsphere`, { recursive: true });    
+            await Deno.writeFile(`${projectName}/.jsphere/server.json`, (new TextEncoder).encode('{}'));
+            await Deno.mkdir(`${projectName}/.jsphere/.tenants`, { recursive: true });    
+            await Deno.writeFile(`${projectName}/.jsphere/.tenants/localhost.json`, (new TextEncoder).encode(getTenantContent()));
+            await Deno.mkdir(`${projectName}/.jsphere/.applications`, { recursive: true });
+            await Deno.writeFile(`${projectName}/.jsphere/.applications/app.json`, (new TextEncoder).encode(getApplicationContent()));
+            await Deno.mkdir(`${projectName}/app/client`, { recursive: true });    
+            await Deno.writeFile(`${projectName}/app/client/index.html`, (new TextEncoder).encode(getClientIndexContent()));
+            await Deno.mkdir(`${projectName}/app/server`, { recursive: true });    
+            await Deno.writeFile(`${projectName}/app/server/index.ts`, (new TextEncoder).encode(getServerIndexContent()));
+            await initRepo({ project: cliConfig.currentProject, repo: '.jsphere' });
+            await initRepo({ project: cliConfig.currentProject, repo: 'app' });
         }
+        await processUseCmd({ _: [ 'use', projectName ] });
     }
     else log.error(`Please provide a project name.`);
-}
-
-// INIT
-async function processInitCmd(cmdArgs) {
-    const projectName = config.currentProject;
-    if (projectName) {
-        await Deno.mkdir(`${projectName}/.jsphere`, { recursive: true });    
-        await Deno.writeFile(`${projectName}/.jsphere/server.json`, (new TextEncoder).encode('{}'));
-        await Deno.mkdir(`${projectName}/.jsphere/.tenants`, { recursive: true });    
-        await Deno.writeFile(`${projectName}/.jsphere/.tenants/localhost.json`, (new TextEncoder).encode(getTenantContent()));
-        await Deno.mkdir(`${projectName}/.jsphere/.applications`, { recursive: true });
-        await Deno.writeFile(`${projectName}/.jsphere/.applications/app.json`, (new TextEncoder).encode(getApplicationContent()));
-        await Deno.mkdir(`${projectName}/app/client`, { recursive: true });    
-        await Deno.writeFile(`${projectName}/app/client/index.html`, (new TextEncoder).encode(getClientIndexContent()));
-        await Deno.mkdir(`${projectName}/app/server`, { recursive: true });    
-        await Deno.writeFile(`${projectName}/app/server/index.ts`, (new TextEncoder).encode(getServerIndexContent()));
-        const envSettings = {
-            projectHost: 'FileSystem',
-            projectRoot: '',
-            projectAuth: '',
-            projectConfig: '.jsphere'
-        }
-        await Deno.writeFile(`${projectName}/.env`, (new TextEncoder).encode(getEnvContent(envSettings)));
-        await processUseCmd({ _: [ 'use', projectName ] });
-    }
-    else log.error('This command is only valid within a project.')
 }
 
 // QUIT
@@ -123,7 +132,7 @@ async function processQuitCmd(cmdArgs) {
 
 // RESET TENANT
 async function processResetCmd(cmdArgs) {
-    const tenant = cmdArgs._[2];
+    const tenant = cmdArgs._[1];
     if (tenant) {
         const response = await fetch(`http://${tenant}/~/resettenant`);
         const result = await response.text();
@@ -136,14 +145,14 @@ async function processResetCmd(cmdArgs) {
 
 // RUN
 async function processRunCmd(cmdArgs) {
-    Deno.chdir(`${Deno.cwd()}/${config.currentProject}`);
-    await runServer();
+    //Deno.chdir(`${Deno.cwd()}/${cliConfig.currentProject}`);
+    await runServer(`./${cliConfig.currentProject}/.env`);
 }
 
 // USE
 async function processUseCmd(cmdArgs) {
     if (cmdArgs._[1] === undefined) {
-        config.currentProject = '';
+        cliConfig.currentProject = '';
         promptText = `JSPHERE>`;
         return;
     }
@@ -154,8 +163,8 @@ async function processUseCmd(cmdArgs) {
     
     for (let entry of dirEntries) {
         if (entry.isDirectory && entry.name.toLowerCase() == projectName) {
-            config.currentProject = entry.name;
-            if (!config.projects[entry.name]) config.projects[entry.name] = {};
+            cliConfig.currentProject = entry.name;
+            cliConfig.currentConfig = await config({ path: `./${projectName}/.env`});
             promptText = `JSPHERE:${entry.name}>`;
             found = true;
         }
@@ -165,46 +174,49 @@ async function processUseCmd(cmdArgs) {
     }
 }
 
+// ********************* UTILITY FUNCTIONS *********************
 
-
-
-
-
-
-async function initFromGitHub(projectRoot, projectConfig) {
-    const process = Deno.run({ 
-        cmd: ['git', 'clone', `https://${config.username}:${config.accessToken}@github.com/${projectRoot}/${projectConfig}.git`],
-        stdout: "piped",
-        stderr: "piped"
-    });
-
-    // copy(process.stdout, Deno.stdout);
-
-    await process.status();
-    const { code } = await process.status();
-
-    // Reading the outputs closes their pipes
-    const rawOutput = await process.stderrOutput
-    const rawError = await process.stderrOutput();
-    process.close();
-   
-    if (code !== 0) {
-        const error = new TextDecoder().decode(rawError);
-        log.error(error);
+async function cloneRepo(config) {
+    // todo: If GitHub/Bitbucket
+    let process;
+    const path = `./${config.project}/${config.repo}`;
+    if (config.accessToken) {
+        process = Deno.run({ 
+            cmd: ['git', 'clone', `https://${config.owner}:${config.accessToken}@github.com/${config.owner}/${config.repo}.git`, path],
+        });
     }
-    
-    await Deno.writeFile(`.env`, (new TextEncoder).encode(getEnvContent()));
-    log.info(`Project initialized.`);
+    else {
+        process = Deno.run({ 
+            cmd: ['git', 'clone', `https://github.com/${config.owner}/${config.repo}.git`, path],
+        });
+    }
+    await process.status();
+    process.close();
 }
 
+async function initRepo(config) {
+    let process;
+    const path = `./${config.project}/${config.repo}`;
+    process = Deno.run({ 
+        cmd: ['git', 'init', path],
+    });
+    await process.status();
+    process.close();
+}
 
 // ********************* CONTENT TEMPLATES *********************
 
 function getEnvContent(envSettings) {
-    const content = `PROJECT_HOST=${envSettings.projectHost}
-PROJECT_ROOT=${envSettings.projectRoot}
-PROJECT_AUTH=${envSettings.projectAuth}
-PROJECT_CONFIG=${envSettings.projectConfig}
+    const content = `USE_LOCAL_CONFIG=${envSettings.uselocalConfig || ''}
+
+LOCAL_CONFIG=${envSettings.localConfig || ''}
+LOCAL_ROOT=${envSettings.localRoot || ''}
+    
+REMOTE_CONFIG=${envSettings.remoteConfig || ''}
+REMOTE_HOST=${envSettings.remoteHost || ''}
+REMOTE_ROOT=${envSettings.remoteRoot || ''}
+REMOTE_AUTH=${envSettings.remoteAuth || ''}
+
 SERVER_HTTP_PORT=80
 `;
     return content;
@@ -225,7 +237,7 @@ function getTenantContent() {
 function getApplicationContent() {
     const content = `{
     "host": {
-        "provider": "FileSystem",
+        "name": "FileSystem",
         "root": "",
         "auth": ""
     },
@@ -260,7 +272,7 @@ function getClientIndexContent() {
 
 function getServerIndexContent() {
     const content = `export async function message(ctx: any) : Promise<any> {
-    return ctx.response.text('JSphere');
+    return ctx.response.text('Hello JSphere');
 }
 message.attributes = { method: 'GET' };
 `;
